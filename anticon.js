0f9d24c980487b54f686b137cebf130a77f7eb6b
Micah Cowan <micah@addictivecode.org>
2015-04-18 14:21:14 -0700

    "Bad guys"

--- anticon.js
+++ anticon.js
@@ -38,72 +38,9 @@ var AntiCon = new (function() {
 
         // Function, not a method. Called via timeout.
         ACG.update = function() {
-            ACG.updateState();
+            ACG.state.update();
             ACG.draw();
             ACG.tmout = window.setTimeout(ACG.update, ACK.MSECS_PER_FRAME);
-        };
-
-        ACG.updateState = function() {
-            var st = ACG.state;
-
-            var now = new Date();
-            var delta = now - st.lastFrameTime;
-            st.lastFrameTime = now;
-            if (delta > ACK.MAX_MSECS_PER_FRAME) {
-                delta = ACK.MAX_MSECS_PER_FRAME;
-            }
-            st.gameElapsed += delta;
-
-            // TODO: speed limit the player slightly?
-            st.playerPos = st.mousePos;
-
-            // WEAPON PHYSICS
-            var weaponPos = st.weaponPos;
-            var weaponMomentum = st.weaponMomentum;
-            var P = AC.Point;
-            var V = AC.Vector;
-
-            // First, apply any existing momentum.
-            if (weaponMomentum.isNonZero) {
-                var weaponMomentumThisFrame = V.scaleBy(weaponMomentum,
-                                                        delta / 1000);
-                weaponPos = V.move(weaponPos, weaponMomentumThisFrame);
-
-                // Apply friction.
-                var friction = ACK.WEAPON_FRICTION * delta/1000
-                if (weaponMomentum.length <= ACK.WEAPON_FRICTION) {
-                    weaponMomentum = new AC.Vector(0, 0);
-                }
-                else {
-                    weaponMomentum = V.lengthen(weaponMomentum,
-                                                -ACK.WEAPON_FRICTION);
-                }
-            }
-
-            // Enforce the tether, and translate that into new momentum.
-            var distVec = P.diff(st.playerPos, weaponPos);
-            if (distVec.length > ACK.TETHER_STRETCH_LENGTH) {
-                distVec = V.scaleTo(distVec,
-                                    distVec.length - ACK.TETHER_STRETCH_LENGTH);
-                weaponPos = P.move(weaponPos, distVec);
-                var distVecPerSec = V.scaleBy(distVec, 1000 / delta);
-                weaponMomentum = V.move(weaponMomentum, distVecPerSec);
-                if (weaponMomentum.length > ACK.MAX_WEAPON_MOMENTUM) {
-                    weaponMomentum = V.scaleTo(weaponMomentum,
-                                               ACK.MAX_WEAPON_MOMENTUM);
-                }
-            }
-            // Is the tether stretched? Adjust momentum as needed.
-            distVec = P.diff(st.playerPos, weaponPos);
-            if (distVec.length > ACK.TETHER_LENGTH) {
-                var tetherMomentum = V.lengthen(distVec, -ACK.TETHER_LENGTH);
-                tetherMomentum = V.scaleBy(tetherMomentum,
-                                           ACK.TETHER_SNAP * 1000 / delta);
-                weaponMomentum = V.move(weaponMomentum, tetherMomentum);
-            }
-            // Save the new values back into state object.
-            st.weaponPos = weaponPos;
-            st.weaponMomentum = weaponMomentum;
         };
 
         ACG.draw = function() {
@@ -112,6 +49,11 @@ var AntiCon = new (function() {
 
             // Clear screen
             scr.clearRect(0, 0, ACK.WIDTH, ACK.HEIGHT);
+
+            // Draw sprites
+            for (var i=0; i < st.sprites.length; ++i) {
+                st.sprites[i].draw(scr);
+            }
 
             // Draw tether
             scr.beginPath();
@@ -150,7 +92,7 @@ var AntiCon = new (function() {
             }
             x -= AC.canvas.offsetLeft;
             y -= AC.canvas.offsetTop;
-            ACG.state.mousePos = new AC.Point(x, y);
+            ACG.state.mousePos = new P(x, y);
 
             ev.stopPropagation();
             ev.preventDefault();
@@ -172,18 +114,97 @@ var AntiCon = new (function() {
         var oL = AC.canvas.offsetLeft;
         var oT = AC.canvas.offsetTop;
         if ('touches' in ev) {
-            startPos = new AC.Point(ev.touches[0].clientX - oL,
+            startPos = new P(ev.touches[0].clientX - oL,
                                     ev.touches[0].clientY - oT);
         }
         else if ('clientX' in ev) {
-            startPos = new AC.Point(ev.clientX - oL, ev.clientY - oT);
+            startPos = new P(ev.clientX - oL, ev.clientY - oT);
         }
 
         S.mousePos  = startPos;
         S.playerPos = startPos;
-        S.weaponPos = AC.Point.move(startPos, ACK.WEAPON_OFFSET);
-        S.weaponMomentum = new AC.Vector(0, 0);
+        S.weaponPos = P.move(startPos, ACK.WEAPON_OFFSET);
+        S.weaponMomentum = new V(0, 0);
+
+        this.sprites = [];
+        this.track = new AC.LevelTrack();
     };
+    AC.GameState.prototype = new (function() {
+        this.addSprite = function(sprite) {
+            this.sprites.push(sprite);
+        };
+        this.update = function() {
+            var st = this;
+
+            var now = new Date();
+            var delta = now - st.lastFrameTime;
+            st.lastFrameTime = now;
+            if (delta > ACK.MAX_MSECS_PER_FRAME) {
+                delta = ACK.MAX_MSECS_PER_FRAME;
+            }
+            st.gameElapsed += delta;
+
+            // TODO: speed limit the player slightly?
+            st.playerPos = st.mousePos;
+
+            // WEAPON PHYSICS
+            var weaponPos = st.weaponPos;
+            var weaponMomentum = st.weaponMomentum;
+
+            // First, apply any existing momentum.
+            if (weaponMomentum.isNonZero) {
+                var weaponMomentumThisFrame = V.scaleBy(weaponMomentum,
+                                                        delta / 1000);
+                weaponPos = V.move(weaponPos, weaponMomentumThisFrame);
+
+                // Apply friction.
+                var friction = ACK.WEAPON_FRICTION * delta/1000
+                if (weaponMomentum.length <= ACK.WEAPON_FRICTION) {
+                    weaponMomentum = new V(0, 0);
+                }
+                else {
+                    weaponMomentum = V.lengthen(weaponMomentum,
+                                                -ACK.WEAPON_FRICTION);
+                }
+            }
+
+            // Enforce the tether, and translate that into new momentum.
+            var distVec = P.diff(st.playerPos, weaponPos);
+            if (distVec.length > ACK.TETHER_STRETCH_LENGTH) {
+                distVec = V.scaleTo(distVec,
+                                    distVec.length - ACK.TETHER_STRETCH_LENGTH);
+                weaponPos = P.move(weaponPos, distVec);
+                var distVecPerSec = V.scaleBy(distVec, 1000 / delta);
+                weaponMomentum = V.move(weaponMomentum, distVecPerSec);
+                if (weaponMomentum.length > ACK.MAX_WEAPON_MOMENTUM) {
+                    weaponMomentum = V.scaleTo(weaponMomentum,
+                                               ACK.MAX_WEAPON_MOMENTUM);
+                }
+            }
+            // Is the tether stretched? Adjust momentum as needed.
+            distVec = P.diff(st.playerPos, weaponPos);
+            if (distVec.length > ACK.TETHER_LENGTH) {
+                var tetherMomentum = V.lengthen(distVec, -ACK.TETHER_LENGTH);
+                tetherMomentum = V.scaleBy(tetherMomentum,
+                                           ACK.TETHER_SNAP * 1000 / delta);
+                weaponMomentum = V.move(weaponMomentum, tetherMomentum);
+            }
+            // Save the new values back into state object.
+            st.weaponPos = weaponPos;
+            st.weaponMomentum = weaponMomentum;
+
+            this.track.run(st);
+            for (var i=0; i < this.sprites.length; ) {
+                var sprite = this.sprites[i];
+                sprite.update(st, delta);
+
+                if (sprite.isDead)
+                    this.sprites.splice(i, 1);
+                else
+                    ++i;
+            }
+        };
+    })();
 
     AC.Point = function(_x, _y) {
         var _length = undefined; // Used in "get" clojure for "length" prop.
@@ -256,6 +277,55 @@ var AntiCon = new (function() {
         return AC.Vector.move(vec, remove);
     };
 
+    var P = AC.Point;
+    var V = AC.Vector;
+
+    // Object class for generating game events (like enemies)
+    AC.LevelTrack = function() {
+        this.nextEvent = [1000, AC.Enemy, new V(ACK.WIDTH / 3, 0),
+                          new V(0, 80)];
+    };
+    AC.LevelTrack.prototype = new (function() {
+        this.run = function(state) {
+            var ev = this.nextEvent;
+            if (ev !== null && state.gameElapsed >= ev[0]) {
+                var ctor = ev[1];
+                ctor = ctor.bind.apply(ctor, [null].concat(ev.slice(2)));
+                var thing = new ctor();
+                state.addSprite(thing);
+                thing.update(state, state.gameElapsed - ev[0]);
+
+                // XXX
+                this.nextEvent[0] += 1000;
+                this.nextEvent[2] = new V(ACK.WIDTH - ev[2].x, 0);
+            }
+        };
+    })();
+
+    AC.Enemy = function(_pos, _vel) {
+        this.position = _pos;
+        this.velocity = _vel;
+        this.isDead = false;
+    };
+    AC.Enemy.prototype = new (function() {
+        this.update = function(state, delta) {
+            var frameVel = V.scaleBy(this.velocity, delta / 1000);
+            this.position = P.move(this.position, frameVel);
+            if (this.position.y > ACK.HEIGHT)
+                this.isDead = true;
+        };
+        this.draw = function(scr) {
+            scr.save();
+            scr.translate(this.position.x, this.position.y);
+            scr.fillStyle = 'blue';
+            scr.fillRect.apply(scr, this.rect);
+            scr.strokeStyle = '1.5px black';
+            scr.strokeRect.apply(scr, this.rect);
+            scr.restore();
+        }
+        this.rect = [-20, -30, 40, 60];
+    })();
+
     AC.defs = new (function() {
         var K = this;
         K.WIDTH = 640;
@@ -267,8 +337,8 @@ var AntiCon = new (function() {
         K.PLAYER_RADIUS = 14;
         K.WEAPON_RADIUS = 12;
 
-        K.PLAYER_START = new AC.Point(K.WIDTH/2, K.HEIGHT/2);
-        K.WEAPON_OFFSET = new AC.Vector(-50, 70);
+        K.PLAYER_START = new P(K.WIDTH/2, K.HEIGHT/2);
+        K.WEAPON_OFFSET = new V(-50, 70);
 
         K.TETHER_LENGTH = 100;
         K.TETHER_STRETCH = 0.85; // fraction of tether length
